d2839078584341f704379427288d8047bd7f5931
McCoy Didericksen <mccoy.did@gmail.com>
2023-03-07 05:03:21 +0800

    adding timeline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/header';
 import Project from './components/project';
+import Timeline from './components/timeline';
 
 function App() {
   return (
     <div className="bg-white dark:bg-stone-800 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
       <Header />
       <Project />
+      <Timeline />
     </div>
   );
 }
---
+++ src/data/timeline.js
@@ -0,0 +1,39 @@
+const timeline = [
+    {
+       year: 'Today',
+       title: 'UC Berkeley Coding Bootcamp',
+       duration: '3 months',
+       details: 
+          "After nearly 2 years of working as a data analyst/engineer, I've decided to make an exciting career pivot to my true passion. I have started a full-time, 12-week coding bootcamp at UC Berkeley and can't wait to become a full-stack developer!",
+    },
+    {
+         year: '2022',
+         title: 'BI Analyst @ Goldman Sachs',
+         duration: '5 months',
+         details:
+             "Used Alteryx to automate crucial business control functions, saving the team on average one hour of daily labor per automation project. Built and maintained critical Tableau dashboards, SQL tables, and Alteryx workflows that delivered strategic insights to external and internal stakeholders across the globe.",
+     },
+     {
+         year: '2022',
+         title: 'Data Engineer @ DoTERRA',
+         duration: '4 months',
+         details:
+             'Developed ETL of Tableau Metadata using GraphQL that drove vital reports to the Analytics team, resulting in improved win/loss analysis of each Tableau asset. Built reconciliation tools in Python that captured 100% of discrepant data points by comparing multiple source and target databases (Snowflake, MySQL, PostgreSQL, Oracle, SAP HANA).',
+     },
+     {
+         year: '2021',
+         title: 'Data Analyst @ Malouf Companies',
+         duration: '9 months',
+         details:
+             "Setup and maintained API integrations to retrieve point-of-sale, inventory, and purchase order data from Amazon, Wayfair, and Walmart to deliver crucial insights to the demand planning team. Wrote custom web scrapers in Python to automate the collection of hard-to-reach web data using the selenium library.",
+     },
+     {
+        year: '2019',
+        title: "Bachelor's Degree in Data Analytics",
+        duration: '3 years',
+        details:
+            'Graduated Magna Cum Laude from Utah State University with a Bachelor of Science in Information Systems and an emphasis in Data Engineering.',
+    },
+ ]
+
+ export default timeline;
