1efa96a38f44c595881768c77e0e37b28ff93b22
McCoy Didericksen <mccoy.did@gmail.com>
2023-03-07 04:24:00 +0800

    adding project details

---
+++ src/data/projects.js
@@ -0,0 +1,25 @@
+const projects = [
+    {
+       title: 'Random Jeopardy',
+       imgUrl: '/assets/jeopardy-game.png',
+       stack: ['HTML', 'CSS', 'JavaScript', 'API'],
+       link: 'https://mccoydidericksen.github.io/jeopardy-game/',
+       sourceLink: 'https://github.com/mccoydidericksen/jeopardy-game'
+    },
+    {
+       title: 'Dropin',
+       imgUrl: '/assets/dropin.png',
+       stack: ['JavaScript', 'Tailwind', 'Express', 'MySQL'],
+       link: 'https://dropin.herokuapp.com/',
+       sourceLink: 'https://github.com/mccoydidericksen/dropin'
+    },
+    {
+       title: 'Algo Trading What-If Simulator',
+       imgUrl: '/assets/coming-soon.jpg',
+       stack: ['MongoDB', 'Express', 'React', 'Node'],
+       link: 'https://github.com/mccoydidericksen',
+       sourceLink: 'https://github.com/mccoydidericksen/what-if-simulator'
+    },
+ ];
+
+ export default projects;
