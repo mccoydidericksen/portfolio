e92e9ae682124b56d746b9036986656ad4fc36be
McCoy Didericksen <mccoy.did@gmail.com>
2023-03-07 08:22:28 +0800

    removing useEffect from app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React, { useEffect } from 'react';
 import Header from './components/header';
 import Project from './components/project';
 import Timeline from './components/timeline';
 import Footer from './components/footer';
 
 function App() {
-
-	
   return (
 		<div className="bg-stone-900 text-stone-300 min-h-screen font-inter">
 			<div className="max-w-5xl w-11/12 mx-auto">
